7e485545aab854e40cde949bdde1f04c51432624
Mindy D Tran <mindytran98@gmail.com>
2024-02-07 15:29:32 -0500

    fix spelling errors

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -4,7 +4,7 @@ import { useSelect, useSelector } from 'react-redux'
 export const PostList = () => {
   const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = post.mat((post) => (
+  const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
